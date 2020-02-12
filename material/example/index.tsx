f99f07eec19da3ee97250928dace14b056f45537
codyaverett <codyaverett@gmail.com>
2020-02-12 00:56:44 -0600

    chore: docx config ts in mdx

--- material/example/index.tsx
+++ material/example/index.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { add } from "./rust/add.rs";
 
 import { Button, Basic } from "../dist";
 
 ReactDOM.render(
   <Basic>
-    <Button>{add(3, 5)}</Button>
+    <Button>OMG!</Button>
   </Basic>,
   document.getElementById("root")
 );
